818db9bd8343640808dce8d4002a27853261836d
kidboi666 <kidboi666@gmail.com>
2025-10-26 20:51:23 +0900

    feat: 헤더 컴포넌트 리팩토링 및 UI 개선
    - 헤더에서 테마 변경 기능을 `DarkModeToggleButton` 컴포넌트로 분리
    - 모바일 상태에서 네비게이션 메뉴를 지원하는 `CollapsibleContent` 추가
    - `NavSection` 컴포넌트를 생성하여 네비게이션 로직 및 UI 분리
    - 버튼 애니메이션에 지연 효과 추가 (`transition.delay` 설정)
    - 불필요한 코드 제거 및 기존 로직 간소화 (`useTheme` 일부 제거)
    - 공용 함수 `cn`을 활용하여 클래스 병합 및 코드 가독성 향상

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -3,17 +3,22 @@
 import {
   BriefcaseBusinessIcon,
   CircleQuestionMarkIcon,
+  ComputerIcon,
+  EllipsisVerticalIcon,
   FileUserIcon,
   HomeIcon,
   MoonIcon,
+  SunIcon,
 } from "lucide-react";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
+  DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuRadioGroup,
   DropdownMenuRadioItem,
@@ -25,6 +30,7 @@ import {
   TooltipContent,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { cn } from "@/lib/utils";
 
 const navItems = [
   { title: "About Me", href: "/about_me", icon: <CircleQuestionMarkIcon /> },
@@ -34,7 +40,6 @@ const navItems = [
 
 export function Header() {
   const pathname = usePathname();
-  const { theme, setTheme } = useTheme();
 
   return (
     <header className="sticky top-2 z-50 mx-2 items-center justify-center rounded-md border border-sidebar-border bg-sidebar-accent/75 px-2 py-2 shadow-xs backdrop-blur-lg">
@@ -51,54 +56,115 @@ export function Header() {
             <p>Home</p>
           </TooltipContent>
         </Tooltip>
-        <ul className="flex gap-2">
-          <li>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="outline" size="icon">
-                  <MoonIcon />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuLabel>테마</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuRadioGroup value={theme} onValueChange={setTheme}>
-                  <DropdownMenuRadioItem value="dark">
-                    Dark
-                  </DropdownMenuRadioItem>
-                  <DropdownMenuRadioItem value="light">
-                    Light
-                  </DropdownMenuRadioItem>
-                  <DropdownMenuRadioItem value="system">
-                    System
-                  </DropdownMenuRadioItem>
-                </DropdownMenuRadioGroup>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </li>
-          {navItems.map(({ title, href, icon }) => {
-            const isActive = pathname === href;
-            return (
-              <li key={title}>
-                <Tooltip>
-                  <TooltipTrigger asChild>
-                    <Button
-                      variant={isActive ? "default" : "outline"}
-                      size="icon"
-                      asChild
-                    >
-                      <Link href={href}>{icon}</Link>
-                    </Button>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>{title}</p>
-                  </TooltipContent>
-                </Tooltip>
-              </li>
-            );
-          })}
-        </ul>
+        <div className="flex gap-2">
+          <DarkModeToggleButton />
+          <CollapsibleContent className="md:hidden" />
+          <NavSection pathname={pathname} className="hidden md:flex" />
+        </div>
       </div>
     </header>
+  );
+}
+
+function CollapsibleContent({ className }: { className?: string }) {
+  const router = useRouter();
+  return (
+    <div className={className}>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" size="icon">
+            <EllipsisVerticalIcon />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          {navItems.map(({ title, href, icon }) => (
+            <DropdownMenuItem key={title} onClick={() => router.push(href)}>
+              {icon}
+              {title}
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+}
+
+function DarkModeToggleButton() {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  function getThemeIcon(currentTheme?: string) {
+    switch (currentTheme) {
+      case "dark":
+        return <MoonIcon />;
+      case "light":
+        return <SunIcon />;
+      default:
+        return <ComputerIcon />;
+    }
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          {mounted ? getThemeIcon(theme) : <ComputerIcon />}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>테마</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuRadioGroup value={theme} onValueChange={setTheme}>
+          <DropdownMenuRadioItem value="dark">
+            <MoonIcon />
+            Dark
+          </DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="light">
+            <SunIcon />
+            Light
+          </DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="system">
+            <ComputerIcon />
+            System
+          </DropdownMenuRadioItem>
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+function NavSection({
+  pathname,
+  className,
+}: {
+  pathname: string;
+  className?: string;
+}) {
+  return (
+    <div className={cn("gap-2", className)}>
+      {navItems.map(({ title, href, icon }) => {
+        const isActive = pathname === href;
+        return (
+          <Tooltip key={title}>
+            <TooltipTrigger asChild>
+              <Button
+                variant={isActive ? "default" : "outline"}
+                size="icon"
+                asChild
+              >
+                <Link href={href}>{icon}</Link>
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>{title}</p>
+            </TooltipContent>
+          </Tooltip>
+        );
+      })}
+    </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         <div className="flex justify-center gap-4">
           <motion.button
             initial={{ opacity: 0, scale: 0.95 }}
-            animate={{ opacity: 1, scale: 1 }}
+            animate={{ opacity: 1, scale: 1, transition: { delay: 0.5 } }}
           >
             <Button variant="outline" asChild>
               <Link href="/portfolio" className="no-underline">
@@ -34,7 +34,7 @@ export default function Home() {
             animate={{
               opacity: 1,
               scale: 1,
-              transition: { duration: 0.3 },
+              transition: { duration: 0.3, delay: 1 },
             }}
           >
             <Button variant="outline" asChild>
