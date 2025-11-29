1cacbf2d18ddbbb068fcc2b8d5befe50aaa1c52e
kidboi666 <kidboi666@gmail.com>
2025-11-29 10:16:29 +0900

    refactor: `ResponsiveGrid` 컴포넌트 개선 및 포트폴리오 문서 업데이트
    - `hasImage` 속성 추가로 포트폴리오 레이아웃 유연성 강화.
    - `onelog.mdx` 및 `mogazoa.mdx`에 컴포넌트 속성 업데이트 및 코드 정리.
    - 반복적인 스타일 제거로 컴포넌트 사용성 및 가독성 향상.
    - 주요 UI 섹션에 설명 추가 및 코드 포맷팅 수정.

--- src/components/shared/responsive_grid.tsx
+++ src/components/shared/responsive_grid.tsx
@@ -5,11 +5,13 @@ interface ResponsiveGridProps extends PropsWithChildren {
   columnRatio?: 1 | 2 | 3 | 4;
   hasCode?: boolean;
   className?: string;
+  hasImage?: boolean;
 }
 
 export function ResponsiveGrid({
   columnRatio = 2,
   hasCode = false,
+  hasImage = false,
   className,
   children,
 }: ResponsiveGridProps) {
@@ -25,7 +27,10 @@ export function ResponsiveGrid({
       className={cn(
         "grid grid-cols-1 gap-8",
         responsiveClasses[columnRatio],
-        hasCode ? "h-[400px]" : "[&>*:nth-child(odd)]:md:text-right",
+        hasCode
+          ? "h-[400px] md:grid-cols-[2fr_1fr]"
+          : "[&>*:nth-child(odd)]:md:text-right",
+        hasImage ? "[&>*:first-child]:justify-self-center" : "",
         className,
       )}
     >
